e080475d2690e1e8a59dd2434d97a8d24061aad4
Vipinbrd <vipinbarode7@gmail.com>
2025-06-18 13:11:15 +0530

    downloaded required dependencies

---
+++ disaster_management_backend/disaster_management_app/src/test/java/com/disaster/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.disaster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
